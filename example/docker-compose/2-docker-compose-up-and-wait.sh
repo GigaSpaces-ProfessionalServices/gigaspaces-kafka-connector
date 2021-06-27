b133a024450f44c3b48c1c2fbac7a4bcf0235a99
Dmitry A <dmitry.a@gmail.com>
2021-06-27 15:58:28 +0300

    Fixed docker compose command incompatible with docker on Ubuntu

--- example/docker-compose/2-docker-compose-up-and-wait.sh
+++ example/docker-compose/2-docker-compose-up-and-wait.sh
@@ -2,7 +2,7 @@
 set -e
 
 # launch the environment
-docker compose up -d
+docker-compose up -d
 
 echo "Waiting for Kafka Connect REST API to be ready. May take 1-2 minutes."
 while [ $(curl -s -o /dev/null -w %{http_code} http://localhost:8083/connectors) -eq 000 ] ; do
